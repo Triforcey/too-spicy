8a5bbc29eaa6ad172c9658f46a882d2f1f703331
Triforcey <superlama01@gmail.com>
2018-04-09 02:36:49 -0600

    implemented discord-routes

--- commands.js
+++ commands.js
@@ -1,8 +1,16 @@
-exports.Commands = class {
-	constructor(client) {
-		this.client = client;
-	}
-	tooSpicy(channel, msgId, req) {
+var CommandSet = require('discord-routes').CommandSet;
+exports.listen = function (client, name) {
+	var commands = new CommandSet(client, name);
+	commands.set('', req => {
+		req.channel.send('<@' + req.author.id + '>\n!toospicy de-spice <message-id>\nTo get a message ID, check out this:\nhttps://support.discordapp.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID-');
+	});
+	commands.set('de-spice', (req, args) => {
+		if (args.length < 1) {
+			req.reply('What message do you wish to de-spice?');
+			return;
+		}
+		var msgId = args[0];
+		var channel = req.channel;
 		channel.fetchMessage(msgId).then((msg) => {
 			var urlRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/;
 			if (!msg.attachments.array().length && !msg.content.match(urlRegex)) {
@@ -14,5 +22,6 @@ exports.Commands = class {
 		}, (err) => {
 			req.reply('I couldn\'t de-spice that message. Are you sure that ID is right?');
 		});
-	}
-};
+	});
+	commands.listen();
+}
--- index.js
+++ index.js
@@ -1,29 +1,13 @@
 var discord = require('discord.js');
 var client = new discord.Client();
-var Commands = require('./commands.js').Commands;
-var commands = new Commands(client);
 var presence = require('./presence.js');
 var presenceTimer = new presence.Timer(client);
+var commands = require('./commands.js');
+
 client.on('ready', function () {
 	presenceTimer.startTimer(300000, 600000);
+	commands.listen(client, '!toospicy');
 	console.log('Bot ready! Logged in as "' + client.user.tag + '".');
-});
-
-client.on('message', function (msg) {
-	var arguments = msg.content.toLowerCase().split(' ');
-	if (arguments.length < 0) return;
-	if (arguments.shift() != '!toospicy') return;
-	switch (arguments.shift()) {
-		case 'de-spice':
-			if (!arguments.length) {
-				msg.reply('you\'re missing arguments!\n!toospicy de-spice <message-id>');
-				break;
-			}
-			commands.tooSpicy(msg.channel, arguments[0], msg);
-			break;
-		default:
-			msg.channel.send('<@' + msg.author.id + '>\n!toospicy de-spice <message-id>\nTo get a message ID, check out this:\nhttps://support.discordapp.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID-');
-	}
 });
 
 client.login(process.env.DISCORD_TOKEN);
